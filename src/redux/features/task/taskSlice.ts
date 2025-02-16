aefa531296c10cee1d5b5b518af2511eb2a6e1f5
Biplob-Rajoan <biplobrajoan2089@gmail.com>
2025-02-16 13:25:02 +0600

    Adds delete and toggle complete functionality to TaskCard component

--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -2,7 +2,6 @@ import { RootState } from "@/redux/store.";
 import { ITask } from "@/types";
 import { createSlice, PayloadAction, nanoid } from "@reduxjs/toolkit";
 
-
 interface InitialState {
   tasks: ITask[];
   filter: "all" | "high" | "medium" | "low";
@@ -16,18 +15,31 @@ const initialState: InitialState = {
 type DraftTask = Pick<ITask, "title" | "description" | "dueDate" | "priority">;
 
 const createTask = (taskData: DraftTask): ITask => {
-    return{id: nanoid(), isCompleted: false, ...taskData}
-}
-
+  return { id: nanoid(), isCompleted: false, ...taskData };
+};
 
 const taskSlice = createSlice({
   name: "task",
   initialState,
   reducers: {
     addTask: (state, action: PayloadAction<ITask>) => {
-        const taskData = createTask(action.payload);
+      const taskData = createTask(action.payload);
       state.tasks.push(taskData);
     },
+
+    toggleCompleteState: (state, action: PayloadAction<string>) => {
+      console.log(action);
+      state.tasks.forEach((task) =>
+        task.id === action.payload
+          ? (task.isCompleted = !task.isCompleted)
+          : task
+      );
+    },
+
+    deleteTask: (state, action: PayloadAction<string>) => {
+      state.tasks = state.tasks.filter((task) => task.id !== action.payload);
+    }
+
   },
 });
 
@@ -39,6 +51,6 @@ export const selectFilter = (state: RootState) => {
   return state.todo.filter;
 };
 
-export const { addTask } = taskSlice.actions;
+export const { addTask, toggleCompleteState, deleteTask } = taskSlice.actions;
 
 export default taskSlice.reducer;
